aa1de256dc5172136545ade1079ad5aed71a21d2
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-08-01 18:22:10 +0100

    use thread local for initializing MessageDigest

--- src/main/java/blippy/crypto/Hashing.java
+++ src/main/java/blippy/crypto/Hashing.java
@@ -5,12 +5,18 @@ import java.security.NoSuchAlgorithmException;
 
 public class Hashing {
 
+  private static final ThreadLocal<MessageDigest> SHA1_MESSAGE_DIGEST_THREAD_LOCAL =
+      ThreadLocal.withInitial(
+          () -> {
+            try {
+              return MessageDigest.getInstance("SHA-1");
+            } catch (final NoSuchAlgorithmException ex) {
+              throw new IllegalStateException("SHA-1 algorithm not available", ex);
+            }
+          });
+
   public static byte[] sha1(final byte[] input) {
-    try {
-      final MessageDigest md = MessageDigest.getInstance("SHA-1");
-      return md.digest(input);
-    } catch (final NoSuchAlgorithmException ex) {
-      throw new RuntimeException(ex);
-    }
+    final MessageDigest md = SHA1_MESSAGE_DIGEST_THREAD_LOCAL.get();
+    return md.digest(input);
   }
 }
